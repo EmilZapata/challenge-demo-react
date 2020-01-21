ec53e379309e25df35d976066c45ccf5483dcd2f
Helmut95 <emilzapata95@gmail.com>
2020-01-20 23:12:06 -0500

    se mejoro la llamada al servicio

--- src/routes/manage/index.js
+++ src/routes/manage/index.js
@@ -1,56 +1,54 @@
-import React, { useState, useMemo, useEffect } from 'react'
-import { Helmet } from 'react-helmet'
-import { connect, useSelector, useDispatch } from 'react-redux'
-import { loadMangeList  } from '../../redux/actions'
-import {
-  Switch,
-  Route,
-} from 'react-router-dom'
+import React, { useState, useMemo, useEffect } from "react";
+import { Helmet } from "react-helmet";
+import { connect, useSelector, useDispatch } from "react-redux";
+import { loadMangeList } from "../../redux/actions";
+import { Switch, Route } from "react-router-dom";
 
 // ui components
-import Grid from '@material-ui/core/Grid';
+import Grid from "@material-ui/core/Grid";
 
 // components
-import { TitleSection } from '../../components'
-import ManageKeyList from './manage-list'
-import ManageKeyDetail from './manage-detail'
-import ManageKeyEdit from './manage-edit'
-import { bindActionCreators } from 'redux'
+import { TitleSection } from "../../components";
+import ManageKeyList from "./manage-list";
+import ManageKeyDetail from "./manage-detail";
+import ManageKeyEdit from "./manage-edit";
+import { bindActionCreators } from "redux";
 
+const Manage = props => {
+  //Initial Dispatch config
+  const dispatch = useDispatch();
 
-const Manage = (props) => {
- //Initial Dispatch config
- const dispatch = useDispatch();
+  const manageActionOperation = useMemo(() => {
+    return bindActionCreators({ loadMangeList }, dispatch);
+  }, [dispatch]);
 
- const manageActionOperation = useMemo(() => {
-   return bindActionCreators({loadMangeList}, dispatch);
- }, [dispatch]);
+  const manageList = useSelector(state => state.manageList);
+  useEffect(() => {
+    if (manageList.data.length === 0) manageActionOperation.loadMangeList();
+    // manageActionOperation.loadMangeList()
+  }, [manageActionOperation]);
 
-const manageList = useSelector(state => state.manageList)
-
-useEffect(() => {
-	if (manageList.data.length === 0) manageActionOperation.loadMangeList()
-	// manageActionOperation.loadMangeList()
-},[])
-
-return (
-	<React.Fragment>
-		<Helmet> <title>Manage | Code Challenge </title> </Helmet>
-		<TitleSection title="Manage data" />
-		<Grid container spacing={3} >
-			<Grid item xs={12} sm={12} md={4} lg={3} >
-				<ManageKeyList/>
-			</Grid>
-			<Grid item xs={12} sm={12} md={8} lg={9} >
-				<Switch>
-			  <Route exact path="/manage" component={ManageKeyDetail} />
-			  <Route exact path="/manage/edit" component={ManageKeyEdit} />
-			</Switch>
-			</Grid>
-		</Grid>
-	</React.Fragment>
-)
-}
+  return (
+    <React.Fragment>
+      <Helmet>
+        {" "}
+        <title>Manage | Code Challenge </title>{" "}
+      </Helmet>
+      <TitleSection title="Manage data" />
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={12} md={4} lg={3}>
+          <ManageKeyList />
+        </Grid>
+        <Grid item xs={12} sm={12} md={8} lg={9}>
+          <Switch>
+            <Route exact path="/manage" component={ManageKeyDetail} />
+            <Route exact path="/manage/edit" component={ManageKeyEdit} />
+          </Switch>
+        </Grid>
+      </Grid>
+    </React.Fragment>
+  );
+};
 
 // class Manage extends React.Component {
 
@@ -98,4 +96,4 @@ return (
 // })
 
 // export default connect(mapStateToProps, mapDispatchToProps)(Manage)
-export default Manage
+export default Manage;
